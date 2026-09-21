3bd5c3a123a88605580f2de2526dc6d0b82cbb62
Balkrishna KC <balkrishnakc565@gmail.com>
2026-09-21 18:55:56 -0500

    Update lambda_function.py

--- 01-lambda-basics/lambda_function.py
+++ 01-lambda-basics/lambda_function.py
@@ -1,1 +1,10 @@
+def lambda_handler(event,context):
+  username=event["username"]
+  action=event["action"]
+  mfa_enabled=event["mfa_enabled"]
 
+
+  if mfa_enabled==False:
+    print(f"Security-alert ‼️ {username} logged in without MFA")
+  else:
+    print(f"Logged in Approved {username}")
